9ca3b0a3a6b9e6db8219132d2415f3f2c161f5da
Kazuka23 <syabanmuhammad12@gmail.com>
2026-03-16 14:33:10 +0700

    Add pattern printing program in E_Mencari_Pola_I.cpp

---
+++ TLX/Pemrograman Dasar dengan C++/05. Perulangan & Percabangan/E_Mencari_Pola_I.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N = 10;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        for (int c = i; c < N; c++) {
+            cout << ".";
+        }
+
+        cout << endl;
+    }
+}
